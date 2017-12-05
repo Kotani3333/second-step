1e2bdaff9841ca55e64e5e73977f79472ff64b59
Kotani3333 <masakari48game@yahoo.co.jp>
2017-12-05 10:18:34 +0900

    fix bug

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -2,12 +2,12 @@
 
 int main(){
 	int i;
-	for(i=1;i<=100,i++){
-		if(i%3=0&&i%5=0){
+	for(i=1;i<=100;i++){
+		if(i%3==0&&i%5==0){
 			printf("fizz buzz\n");
-		}else if(i%3=0){
+		}else if(i%3==0){
 			printf("fizz\n");
-		}else if(i%5=0){
+		}else if(i%5==0){
 			printf("buzz\n");
 		}else{
 			printf("%d\n",i);
