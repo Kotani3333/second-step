d181b5e99adc70f23898900a9ee1abfb803567a9
chikoyoshi <chikoyoshi-21141@outlook.jp>
2017-12-05 10:19:24 +0900

    コメント追加

--- fib.rb
+++ fib.rb
@@ -1,13 +1,13 @@
-#!/usr/bin/ruby 
+#!/usr/bin/ruby -Ku
 
-def fib(num)
+def fib(num)			#フィボナッチ数列を求める関数です
 	if (num == 1)
 		return 1
 	end
 	if (num == 2) 
 		return 1
 	end
-	return fib(num-1) + fib(num-2)
+	return fib(num-1) + fib(num-2)		#1つ前の項と2つ前の項を足してます
 end
 
 p fib(10)
